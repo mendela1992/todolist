13460f41e41a75c0648f1fb51d0f5bc072669c1e
PFCPT-NKI <ndick4@gmail.com>
2022-01-10 07:59:56 +0200

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreTaskRequest $request
+     * @return TaskResource
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'completed' => $request->completed,
+        ]);
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Task $task
+     * @return TaskResource
+     */
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateTaskRequest $request
+     * @param Task $task
+     * @return TaskResource
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->all());
+        return new TaskResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Task $task
+     * @return JsonResponse
+     */
+    public function destroy(Task $task): JsonResponse
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'completed' => $this->completed === 1 ? "Oui" : "Non",
+            'created_at' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->updated_at->diffForHumans(),
+        ];
+    }
+}
